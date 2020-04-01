18e8e4a5ec989d31e8e963f0afbff5f3cfac2972
Patty <divyapattisapu@gmail.com>
2020-04-01 17:37:41 +0530

    task1.2

--- task1_4.py
+++ task1_4.py
@@ -34,8 +34,8 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1,32, 5)
         #self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(128, 10)
-        #self.fc2 = nn.Linear(784, 10)
+        self.fc1 = nn.Linear(3686, 128)
+        self.fc2 = nn.Linear(128, 10)
         #self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
@@ -44,7 +44,7 @@ class Net(nn.Module):
         #x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
-        x = F.softmax(x,dim=0)
+        x = F.softmax(self.fc2(x),dim=0)
         #x = F.softmax(self.fc2(x),dim=0)
         #print(x.shape)
         #x = self.fc3(x)
